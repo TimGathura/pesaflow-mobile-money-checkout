85b97ccf5dde34622936ff26448e3caa9c1de1e7
TimGathura <timgathura@gmail.com>
2024-09-26 16:33:22 +0300

    all mobile sizes responsive, navigation working, alpine issue

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,16 +17,22 @@
 
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
 import "phoenix_html"
+
+
 // Establish Phoenix Socket and LiveView configuration.
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
+import Alpine from 'alpinejs'
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {
   longPollFallbackMs: 2500,
   params: {_csrf_token: csrfToken}
 })
+
+window.Alpine = Alpine
+Alpine.start()
 
 // Show progress bar on live navigation and form submits
 topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
